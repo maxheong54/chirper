48dc7aeafb6f619234fb03a2dcdf452d20240dc7
maxheong54 <maxheong54@gmail.com>
2025-10-21 23:24:21 +0300

    Add login and logout

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 
 Route::get('/welcome', function () {
     return view('welcome');
@@ -22,17 +24,20 @@ Route::middleware('auth')->group(function () {
     ->only(['store', 'edit', 'update', 'destroy']);
 });
 
-// Route::post('/chirps', [ChirpController::class, 'store']);
-// Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
-// Route::put('chirps/{chirp}', [ChirpController::class, 'update']);
-// Route::delete('chirps/{chirp}', [ChirpController::class, 'destroy']);
-
-// Route::resource('chirps', ChirpController::class)
-//     ->only(['store', 'edit', 'update', 'destroy']);
-
 Route::view('/register', 'auth.register')
     ->middleware('guest')
     ->name('register');
 
 Route::post('/register', Register::class)
     ->middleware('guest');
+
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', Login::class)
+    ->middleware('guest');
+
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
